97293e5f5cf332910e43217805b92ce996976a25
Pierre Abeille <pierre.abeille@g-mail.fr>
2022-06-28 16:30:54 +0200

    Update Article to ArticlePage

--- FRONT/wiki_front/pages/article/[id].js
+++ FRONT/wiki_front/pages/article/[id].js
@@ -2,35 +2,39 @@ import styles from '../../styles/components.module.scss';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import Article from '../../components/article';
 
+//Make a ArticlePage component that displays an Article component.
 
-const Article = () => {
-    const router = useRouter();
-    const { id } = router.query
-    const [article, setArticle] = useState(null);
-    const [loading, setLoading] = useState(true);
+// const Article = () => {
+//     const router = useRouter();
+//     const { id } = router.query
+//     const [article, setArticle] = useState(null);
+//     const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const res = await fetch(`/api/article/${id}`);
-            const data = await res.json();
-            setArticle(data);
-            setLoading(false);
-        }
-        fetchData();
-    }, []);
+//     useEffect(() => {
+//         const fetchData = async () => {
+//             const res = await fetch(`/api/article/${id}`);
+//             const data = await res.json();
+//             setArticle(data);
+//             setLoading(false);
+//         }
+//         fetchData();
+//     }, []);
 
-    if (loading) {
-        return <p>Loading... {id}</p>
-    }
+//     if (loading) {
+//         return <p>Loading... {id}</p>
+//     }
+// }
 
+const ArticlePage = ({id}) => {
     return (
         <div>
             
             
-            <h1>{article.title}</h1>
+            {/* <h1>{article.title}</h1>
             <p>{article.content}</p>
-            <p>{article.version}</p>
+            <p>{article.version}</p> */}
             
             <Link href={`edit/${article._id}`}>
                 <a>Editer cet article</a>
@@ -40,4 +44,4 @@ const Article = () => {
 }
 
 
-export default Article;
+export default ArticlePage;
