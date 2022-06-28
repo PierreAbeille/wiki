c532b06f657a47ec3a5d9efb6bbacb2076aa803b
Pierre Abeille <pierre.abeille@g-mail.fr>
2022-06-28 20:06:41 +0200

    Update index page

--- FRONT/wiki_front/pages/index.js
+++ FRONT/wiki_front/pages/index.js
@@ -1,9 +1,34 @@
 import styles from '../styles/pages.module.scss'
-import Navbar from '../components/navbar'
+import Sidenav from '../components/sidenav';
+import Article from '../components/article';
 
 export default function Home() {
+  //get the last three articles
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('/api/articles');
+      const data = await res.json();
+      setArticles(data.articles);
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>
+  }
+  
   return (
     <div className={styles.container}>
+      <Sidenav />
+      //display the last three articles
+      <div className={styles.articles}>
+        {articles.map(article => (
+          <Article key={article.id} article={article} />
+        ))}
+      </div>
     </div>
   )
 }
