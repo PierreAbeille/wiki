6fea0232583af0cc7aa31bad7ff4611461f82c07
Pierre Abeille <pierre.abeille@g-mail.fr>
2022-06-29 22:46:15 +0200

    Display Article component for api article/id

--- FRONT/wiki_front/components/article.js
+++ FRONT/wiki_front/components/article.js
@@ -1,16 +1,41 @@
-import styles from '../styles/components.module.scss';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
 
-
-const Article = (article) => {
+export default function Article({article}) {
     return (
         <div>
             <h1>{article.title}</h1>
             <p>{article.content}</p>
+            <p>{article.version}</p>
+
+            <Link href={`edit/${article._id}`}> 
+                    <a>Editer cet article</a>
+            </Link>
         </div>
+
     )
 }
 
-export default Article;
+export async function getStaticProps({
+    params
+}) {
+    const article = await fetch(`http://localhost:3000/articles/${params.id}`).then(res => res.json());
+    return {
+        props: {
+            article
+        }
+    }
+}
+
+export async function getStaticPaths() {
+    const res = await fetch(`http://localhost:3000/articles`);
+    const data = await res.json();
+    const paths = data.map(article => ({
+        params: {
+            id: article._id
+        }
+    }));
+    return {
+        paths,
+        fallback: false
+    };
+}
--- FRONT/wiki_front/pages/article/[id].js
+++ FRONT/wiki_front/pages/article/[id].js
@@ -1,24 +1,9 @@
-import styles from '../../styles/components.module.scss';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import Article from '../../components/article';
 
-// Make a ArticlePage component that displays an Article component.
 export default function ArticlePage ({article}) {
     return (
         <div>
-            <h1>{article[0].title}</h1>
-            <p>{article[0].content}</p>
-            <p>{article[0].version}</p>
-            
-            <Link href={`edit/${article[0]._id}`}> 
-                <a>Editer cet article</a>
-            </Link>
-
-            <br/><br/>
-            <Link href={`versions/${article[0].title}`}> 
-                <a>Voir les versions de cet article</a>
-            </Link>
+            <Article article={article} />
         </div>
     )
 }
